4b0f07b702441925978217b1823e4eb07a9e3fd5
beantz123 <trizmours2002@gmail.com>
2025-08-24 16:34:03 -0300

    feat: usando form request

---
+++ app/Http/Requests/ValidationAgendamento.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class ValidationAgendamento extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+
+            'data_consulta' => 'required|date',
+            'relatorio_consulta' => 'nullable|string|max:240',
+            'user_patients_id' => 'required|integer|exists:users,id'
+        ];
+    }
+
+    public function messages() {
+
+        return [
+            'required' => 'O campo :attribute deve ser preenchido',
+            'relatorio_consulta.max' => 'O campo nome deve ter no máximo 240 caracteres',
+            'relatorio_consulta.string' => 'O campo de relatorio deve ser do tipo string',
+            'data_consulta.date' => 'A data de consulta inválida',
+            'user_patients_id.exists' => 'O id de usuario não existe',
+            'user_patients_id.integer' => 'O id de usuario precisa ser um numero inteiro'
+        ];
+
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'message' => 'Erros de validação',
+                'erros' => $validator->errors(),
+            ], 420)
+        );
+    }
+}
---
+++ app/Http/Requests/ValidationAgendamentoProcedimento.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class ValidationAgendamentoProcedimento extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'id_procedimento' => 'required|integer|exists:procedimento,id'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'id_procedimento.exists' => 'O id fornecido não existe na tabela de procedimentos',
+            'id_procedimento.integer' => 'O id precisa ser um número inteiro'
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'message' => 'Erros de validação',
+                'erros' => $validator->errors(),
+            ], 420)
+        );
+    }
+}
